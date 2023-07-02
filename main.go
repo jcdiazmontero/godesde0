11503fca11c6fdec892d484e32568d0746ec45a7
Juan Carlos <undivided@gmail.com>
2023-07-02 18:24:05 +0200

    .
    .

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jcdiazmontero/godesde0/variables"
+)
 
 func main() {
 	fmt.Println("HOLA MUNDOOO!!!")
+	variables.MuestraEnteros()
 }
---
+++ variables/enteras.go
@@ -0,0 +1,9 @@
+package variables
+
+import "fmt"
+
+func MuestraEnteros() {
+	num1 := 90;
+
+	fmt.Print(num1)
+}
